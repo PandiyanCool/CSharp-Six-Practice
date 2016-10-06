352b075f943436e22d88629f312e80ce224e23a3
Pandiyan <pandiyanit7@gmail.com>
2016-10-06 23:01:23 +0530

    Initial commit

---
+++ 20161006/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public int MultipleTwo (int x) => x*2;
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public static void Main()
+        {
+            
+            StringBuilder st = null;
+            string output = st?.ToString();
+
+            Console.WriteLine("Hello friend! {0}", st);
+        }
+    }
+}
